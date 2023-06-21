413bb0a0c86110d196aaa503d61f4e1815978536
GiveForFood <allen22052000@gmail.com>
2023-06-22 00:23:45 +0300

    changes

--- resources/views/organization/index.blade.php
+++ resources/views/organization/index.blade.php
@@ -12,12 +12,12 @@
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <!-- Nucleo Icons -->
-    <link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
-    <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
+    <link href="{{asset('css/nucleo-icons.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/nucleo-svg.css')}}" rel="stylesheet" />
     <!-- Popper -->
     <script src="https://unpkg.com/@popperjs/core@2"></script>
     <!-- Main Styling -->
-    <link href="./assets/css/soft-ui-dashboard-tailwind.css?v=1.0.5" rel="stylesheet" />    
+    <link href="{{asset('css/dashboard-tailwind.css?v=1.0.5')}}" rel="stylesheet" />    
     <!-- Nepcha Analytics (nepcha.com) -->
     <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
     <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
@@ -1172,11 +1172,11 @@
     </div>
   </body>
   <!-- plugin for charts  -->
-  <script src="./assets/js/plugins/chartjs.min.js" async></script>
+  <script src="{{asset('js/org/plugins/chartjs.min.js')}}" async></script>
   <!-- plugin for scrollbar  -->
-  <script src="./assets/js/plugins/perfect-scrollbar.min.js" async></script>
+  <script src="{{asset('js/org/plugins/perfect-scrollbar.min.js')}}" async></script>
   <!-- github button -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- main script file  -->
-  <script src="./assets/js/soft-ui-dashboard-tailwind.js?v=1.0.5" async></script>
+  <script src="{{asset('js/org/dashboard-tailwind.js?v=1.0.5')}}" async></script>
 </html>

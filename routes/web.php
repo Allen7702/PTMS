d96336dec61995c8a467d2ef8b45e52247d87961
GiveForFood <allen22052000@gmail.com>
2023-06-21 23:45:27 +0300

    organization

--- routes/web.php
+++ routes/web.php
@@ -42,4 +42,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/org', function () {
+    return view('organization.index');
+});
+
 require __DIR__.'/auth.php';

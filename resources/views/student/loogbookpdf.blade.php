f5c2ac34b56bf7b5e6aab9222815e7fa3f787a7d
johnson419 <johnsonfredrick577@.com>
2023-06-23 02:11:30 +0300

    minor improvements in logbook

--- resources/views/student/loogbookpdf.blade.php
+++ resources/views/student/loogbookpdf.blade.php
@@ -1,77 +1,96 @@
  <!-- Scripts -->
  @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-<div class="container mx-auto p-4">
-    <div class="align-center">
-    <h3 class="text-lg font-bold mb-4">UNIVERSITY OF DAR ES SALAAM</h3>
-    <h3 class="text-lg font-bold mb-4">COLLEGE OF INFORMATION AND COMMUNICATION TECHNOLOGIES</h3>
-    <h3 class="text-lg font-bold mb-4">DEPARTMENT OF ………………………………………………………….</h3>
-    <h3 class="text-lg font-bold mb-4">PRACTICAL TRAINING LOG - BOOK</h3>
+ <div class="flex flex-col p-4">
+   <div class="flex items-center justify-center mb-auto">
+     <div class="mb-2">
+       <h3 class="text-lg font-bold mb-2">UNIVERSITY OF DAR ES SALAAM</h3>
+       <h3 class="text-lg font-bold mb-2">COLLEGE OF INFORMATION AND COMMUNICATION TECHNOLOGIES</h3>
+       <h3 class="text-lg font-bold mb-2">DEPARTMENT OF ………………………………………………………….</h3>
+       <h3 class="text-lg font-bold mb-2">PRACTICAL TRAINING LOG - BOOK</h3>
+     </div>
 
-    </div>
+   </div>
+
+   <div class="flex items-center justify-between mb-4">
+     <p class="mb-2">STUDENTS NAME: ……………………… REG. NO: …………………………………</p>
+     <p class="mb-2">COMPANY/INSTITUTION: ………………………………………………………………</p>
+   </div>
+
+   <table class="w-full border border-black mb-4">
+     <thead>
+       <tr>
+         <td class="border border-black px-4 py-2">
+           WEEK NO:………
+         </td>
+         <td class="border border-black px-4 py-2">
+
+           FROM:…………… TO:…………………… (date)
+         </td>
+
+       </tr>
+       <tr>
+         <th class="border border-black px-4 py-2">DAY / DATE</th>
+         <th class="border border-black px-4 py-2">ACTIVITY</th>
+       </tr>
+     </thead>
+     <tbody>
+       <tr>
+         <td class="border border-black  px-4 py-2">Monday</td>
+         <td class="border border-black px-4 py-2"></td>
+       </tr>
+       <tr>
+         <td class="border border-black px-4 py-2">Tuesday</td>
+         <td class="border border-black px-4 py-2"></td>
+       </tr>
+       <tr>
+         <td class="border border-black px-4 py-2">Wednesday</td>
+         <td class="border border-black px-4 py-2"></td>
+       </tr>
+       <tr>
+         <td class="border border-black px-4 py-2">Thursday</td>
+         <td class="border border-black px-4 py-2"></td>
+       </tr>
+       <tr>
+         <td class="border border-black px-4 py-2">Friday</td>
+         <td class="border border-black px-4 py-2"></td>
+       </tr>
+     </tbody>
+   </table>
+
+   <h3 class="text-lg font-bold mb-2">Details Of the Main Job of the Week</h3>
+
+   <div class="operation mb-4">
+     <table class="w-full border border-black">
+       <tr>
+         <th class="border border-black px-4 py-2">Operation</th>
+         <th class="border border-black px-4 py-2">Machinery/Tools Used</th>
+       </tr>
+       <tr>
+         <td class="border border-black px-4 py-2 w-3/4"></td>
+         <td class="border border-black px-4 py-2"></td>
+       </tr>
+
+       <tr>
+         <td class="flex  border border-black px-4 py-2 ">Comments from Industrial Supervisor</td>
+         <td class="border border-black px-4 py-2"></td>
+       </tr>
+       <tr>
+         <td class="border border-black px-4 py-2 w-3/4">Name: …………………………………………………</td>
+         <td>Signature: ………………</td>
+       </tr>
+     </table>
     
-    <div class="details mb-4">
-      <p class="mb-2">STUDENTS NAME: ………………………. REG. NO…………………………………</p>
-      <p class="mb-2">COMPANY/INSTITUTION: …….….………………………………………………………</p>
-      <p class="mb-2">WEEK NO……… FROM…………….. TO……………………. (date)</p>
-    </div>
+   </div>
 
-    <table class="w-full border border-black mb-4">
-      <thead>
-        <tr>
-          <th class="border border-black px-4 py-2">DAY / DATE</th>
-          <th class="border border-black px-4 py-2">ACTIVITY</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td class="border border-black  px-4 py-2">Monday</td>
-          <td class="border border-black px-4 py-2"></td>
-        </tr>
-        <tr>
-          <td class="border border-black px-4 py-2">Tuesday</td>
-          <td class="border border-black px-4 py-2"></td>
-        </tr>
-        <tr>
-          <td class="border border-black px-4 py-2">Wednesday</td>
-          <td class="border border-black px-4 py-2"></td>
-        </tr>
-        <tr>
-          <td class="border border-black px-4 py-2">Thursday</td>
-          <td class="border border-black px-4 py-2"></td>
-        </tr>
-        <tr>
-          <td class="border border-black px-4 py-2">Friday</td>
-          <td class="border border-black px-4 py-2"></td>
-        </tr>
-      </tbody>
-    </table>
 
-    <h3 class="text-lg font-bold mb-2">Details Of the Main Job of the Week</h3>
 
-    <div class="operation mb-4">
-      <table class="w-full border border-black">
-        <tr>
-          <th class="border border-black px-4 py-2">Operation</th>
-          <th class="border border-black px-4 py-2">Machinery/Tools Used</th>
-        </tr>
-        <tr>
-          <td class="border border-black px-4 py-2 w-3/4" ></td>
-          <td class="border border-black px-4 py-2"></td>
-        </tr>
-        
-      </table>
-    </div>
+   <div class="diagram">
+     <h3 class="text-lg font-bold mb-2">Detailed Diagram of the Main Job</h3>
+     <div class=" border border-black w-800px h-400px">
+       <img src="https://via.placeholder.com/800x400" alt="">
+     </div>
 
-    <div class="comments mb-4">
-      <p class="font-bold">Comments from Industrial Supervisor</p>
-      <p>Name: ………………………………………………… Signature: ………………</p>
-    </div>
-
-    <div class="diagram">
-      <h3 class="text-lg font-bold mb-2">Detailed Diagram of the Main Job</h3>
-      <div class="diagram_area border border-black max-w-800px h-400px"></div>
-
-      <!-- Add your diagram image or code here -->
-    </div>
-  </div>
+     <!-- Add your diagram image or code here -->
+   </div>
+ </div>

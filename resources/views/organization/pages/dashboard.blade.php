3f577ae678df3ceb1c9be50019d390e792e6e101
GiveForFood <allen22052000@gmail.com>
2023-06-25 20:07:55 +0300

    changes

--- resources/views/organization/pages/dashboard.blade.php
+++ resources/views/organization/pages/dashboard.blade.php
@@ -1,8 +1,111 @@
 @extends('organization.layout')
 
 @section('content')
+<div class="w-full px-6 py-6 mx-auto">
+  <!-- Student Statistics -->
+  <div class="flex flex-wrap -mx-3">
+    <!-- Total Students -->
+    <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+      <div class="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
+        <div class="flex-auto p-4">
+          <div class="flex flex-row -mx-3">
+            <div class="flex-none w-2/3 max-w-full px-3">
+              <div>
+                <p class="mb-0 font-sans text-sm font-semibold leading-normal">Total Students</p>
+                <h5 class="mb-0 font-bold">
+                  5,000
+                  <span class="text-sm leading-normal font-weight-bolder text-lime-500">+10%</span>
+                </h5>
+              </div>
+            </div>
+            <div class="px-3 text-right basis-1/3">
+              <div class="inline-block w-12 h-12 text-center rounded-lg bg-gradient-to-tl from-purple-700 to-pink-500">
+                <i class="ni leading-none ni-graduation-cap text-lg relative top-3.5 text-white"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Organizations Offering Training -->
+    <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+      <div class="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
+        <div class="flex-auto p-4">
+          <div class="flex flex-row -mx-3">
+            <div class="flex-none w-2/3 max-w-full px-3">
+              <div>
+                <p class="mb-0 font-sans text-sm font-semibold leading-normal">Organizations Offering Training</p>
+                <h5 class="mb-0 font-bold">
+                  50
+                  <span class="text-sm leading-normal font-weight-bolder text-lime-500">+25%</span>
+                </h5>
+              </div>
+            </div>
+            <div class="px-3 text-right basis-1/3">
+              <div class="inline-block w-12 h-12 text-center rounded-lg bg-gradient-to-tl from-purple-700 to-pink-500">
+                <i class="ni leading-none ni-briefcase-24 text-lg relative top-3.5 text-white"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Available Training Opportunities -->
+    <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+      <div class="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
+        <div class="flex-auto p-4">
+          <div class="flex flex-row -mx-3">
+            <div class="flex-none w-2/3 max-w-full.
+            px-3">
+            <div>
+            <p class="mb-0 font-sans text-sm font-semibold leading-normal">Available Training Opportunities</p>
+            <h5 class="mb-0 font-bold">
+            200
+            <span class="text-sm leading-normal font-weight-bolder text-lime-500">+15%</span>
+            </h5>
+            </div>
+            </div>
+            <div class="px-3 text-right basis-1/3">
+            <div class="inline-block w-12 h-12 text-center rounded-lg bg-gradient-to-tl from-purple-700 to-pink-500">
+            <i class="ni leading-none ni-laptop text-lg relative top-3.5 text-white"></i>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            <!-- Training Completion Rate -->
+<div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
+  <div class="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
+    <div class="flex-auto p-4">
+      <div class="flex flex-row -mx-3">
+        <div class="flex-none w-2/3 max-w-full px-3">
+          <div>
+            <p class="mb-0 font-sans text-sm font-semibold leading-normal">Training Completion Rate</p>
+            <h5 class="mb-0 font-bold">
+              85%
+              <span class="text-sm leading-normal font-weight-bolder text-lime-500">+5%</span>
+            </h5>
+          </div>
+        </div>
+        <div class="px-3 text-right basis-1/3">
+          <div class="inline-block w-12 h-12 text-center rounded-lg bg-gradient-to-tl from-purple-700 to-pink-500">
+            <i class="ni leading-none ni-chart-bar-32 text-lg relative top-3.5 text-white"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+
+
       <!-- cards -->
-      <div class="w-full px-6 py-6 mx-auto">
+      {{-- <div class="w-full px-6 py-6 mx-auto">
         <!-- row 1 -->
         <div class="flex flex-wrap -mx-3">
           <!-- card1 -->
@@ -298,6 +401,6 @@
 
     </div>
        <!-- end cards -->
-    </main>
+    </main> --}}
 
 @endsection

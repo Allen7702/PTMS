d6e1251afea5a7632996bb51100f62cc61dba303
Johnson Fredrick <johnsonfredrick577@gmail.com>
2023-07-06 10:22:48 +0300

    improvements

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -46,10 +46,10 @@
         </div>
         <div class="dropdown-main">
        <ul class="nav-links">
-        <li><a href="#" class="text-lg mx-4">Student</a></li>
-        <li><a href="#">Location Preference</a></li>
+        <li><a href="{{route('dashboard.index')}}" class="text-lg mx-4">Student</a></li>
+        <li><a href="{{route('dashboard.index')}}">Location Preference</a></li>
         <li><a href="{{route('student.company')}}">PT Allocation</a></li>
-        <li><a href="#">Resources</a></li>
+        <li><a href="{{route('dashboard.index')}}">Resources</a></li>
        </ul>
     </div>
       
@@ -68,49 +68,49 @@
         <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
-            <div class="media">
+            <!-- <div class="media">
               <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
-                  Brad Diesel
+                  Allen Andrew
                   <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
                 </h3>
-                <p class="text-sm">Call me whenever you can...</p>
+                <p class="text-sm">submitted logbook week 1.</p>
                 <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
               </div>
-            </div>
+            </div> -->
             <!-- Message End -->
           </a>
           <div class="dropdown-divider"></div>
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
-            <div class="media">
+            <!-- <div class="media">
               <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
-                  John Pierce
+                  Allen Andrew
                   <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
                 </h3>
-                <p class="text-sm">I got your message bro</p>
+                <p class="text-sm">submitted logbook week 1</p>
                 <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
               </div>
-            </div>
+            </div> -->
             <!-- Message End -->
           </a>
           <div class="dropdown-divider"></div>
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
-            <div class="media">
+            <!-- <div class="media">
               <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
-                  Nora Silvester
+                 Allen Andrew
                   <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
                 </h3>
-                <p class="text-sm">The subject goes here</p>
+                <p class="text-sm">submitted logbook week 1</p>
                 <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
               </div>
-            </div>
+            </div> -->
             <!-- Message End -->
           </a>
           <div class="dropdown-divider"></div>
@@ -208,20 +208,6 @@ $('#logout-form').submit();
     })
   </script>
 
-  {{-- toastr js --}}
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.1/js/toastr.js"></script>
-
-  <script>
-      $(document).ready(function() {
-          toastr.options.timeOut = 10000;
-          @if (Session::has('error'))
-              toastr.error('{{ Session::get('error') }}');
-          @elseif(Session::has('success'))
-              toastr.success('{{ Session::get('success') }}');
-          @endif
-      });
-
-  </script>
 
 <script src="{{asset('js/script.js')}}"></script>
 <script src="{{asset('js/main.js')}}"></script>

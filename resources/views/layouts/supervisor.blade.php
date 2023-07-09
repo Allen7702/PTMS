f479314e3dfce68f51cdcf558ed9af82fb34d1d8
Johnson Fredrick <johnsonfredrick577@gmail.com>
2023-07-09 22:52:21 +0300

    ui improvements

--- resources/views/layouts/supervisor.blade.php
+++ resources/views/layouts/supervisor.blade.php
@@ -62,7 +62,7 @@
           <a href="#" class="dropdown-item">
             <!-- Message Start -->
             <div class="media">
-              <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+              <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-4 img-circle">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
                   Brad Diesel

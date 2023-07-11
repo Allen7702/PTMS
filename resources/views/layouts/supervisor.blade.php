d606d7fca19393c8c6ee01f38534f7e51698cf73
Johnson Fredrick <johnsonfredrick577@gmail.com>
2023-07-12 00:38:21 +0300

    improved supervisor dashboard

--- resources/views/layouts/supervisor.blade.php
+++ resources/views/layouts/supervisor.blade.php
@@ -81,10 +81,10 @@
               <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
-                  John Pierce
+                  Valence Lugimbana
                   <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
                 </h3>
-                <p class="text-sm">I got your message bro</p>
+                <p class="text-sm">This system is so good</p>
                 <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
               </div>
             </div>
